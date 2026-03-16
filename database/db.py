f0bab1875b25aa90724fba30de058d25d883488a
LesediLebotsa <lede.lebotsa1598@gmail.com>
2026-03-17 00:12:43 +0200

    Updated database setup and added test database. Fixed employee display errors

--- database/db.py
+++ database/db.py
@@ -1,7 +1,10 @@
 import sqlite3
 from config.config import DATABASE_PATH
-print("using database:", DATABASE_PATH)
-def get_connection():
-    conn = sqlite3.connect(DATABASE_PATH)
+from flask import current_app
+
+def get_connection(db_path=None):
+    if db_path is None:
+        db_path = DATABASE_PATH
+    conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
